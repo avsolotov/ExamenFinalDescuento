89175eac428517fda084bc4a5de9306e541e0a52
avsolotov <50112302+avsolotov@users.noreply.github.com>
2019-05-21 10:43:07 -0500

    Add files via upload

---
+++ ExamenFinalAndresValencia/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ExamenFinalAndresValencia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnprocesar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnx = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnprocesar
+            // 
+            this.btnprocesar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnprocesar.Location = new System.Drawing.Point(33, 317);
+            this.btnprocesar.Name = "btnprocesar";
+            this.btnprocesar.Size = new System.Drawing.Size(100, 23);
+            this.btnprocesar.TabIndex = 0;
+            this.btnprocesar.Text = "TERAPIAS";
+            this.btnprocesar.UseVisualStyleBackColor = true;
+            this.btnprocesar.Click += new System.EventHandler(this.btnprocesar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(33, 95);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 212);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PROMEDIO :";
+            // 
+            // btnx
+            // 
+            this.btnx.BackColor = System.Drawing.Color.Crimson;
+            this.btnx.Location = new System.Drawing.Point(139, 0);
+            this.btnx.Name = "btnx";
+            this.btnx.Size = new System.Drawing.Size(26, 23);
+            this.btnx.TabIndex = 0;
+            this.btnx.UseVisualStyleBackColor = false;
+            this.btnx.Click += new System.EventHandler(this.btnx_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "SOLOTOVMEDIA \r\nCopyRigthsReserve 2019";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(164, 352);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnx);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnprocesar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnprocesar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnx;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ExamenFinalAndresValencia/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ExamenFinalAndresValencia
+{
+    public partial class Form1 : Form
+    {
+        int z, j, limite;
+        string informe;
+        double netoterapia, promedio, valorterapia, descuento1;
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnx_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+    
+        }
+
+        private void btnprocesar_Click(object sender, EventArgs e)
+        {
+            do
+            {
+                limite = int.Parse(Interaction.InputBox("Cantidad de Pacientes : "));
+                if (limite <= 0)
+                {
+                    MessageBox.Show("Numero de pacientes no validos, Intente Otra vez.");
+
+                }
+            } while (limite <= 0);
+
+            string[] nomPaciente = new string[limite];
+            double[] descuento = new double[limite];
+
+            for (z = 0; z < nomPaciente.Length; z++)
+            {
+                nomPaciente[z] = Interaction.InputBox("Nombre del paciente: ");
+                valorterapia = double.Parse(Interaction.InputBox("Valor de la consulta: "));
+                if (valorterapia < 500 || valorterapia > 1000) {
+                    MessageBox.Show("Valor de Consulta no validos, Intente Otra vez.");
+                }
+                descuento1 = calculardescuento(valorterapia, descuento1);
+                descuento[z] = descuento1;
+                netoterapia += valorterapia;
+            }
+
+            informe = generarinforme(nomPaciente, descuento);
+            promedio = netoterapia / limite;
+            MessageBox.Show("Promedio: " + promedio);
+            MessageBox.Show("INFORME: " + informe);
+            textBox1.Text = promedio.ToString();
+            richTextBox1.Text = ("\nINFORME: " + "\n" + informe);
+        }
+       
+        private double calculardescuento(double valor_terapia, double descuento1)
+        {
+            if (valor_terapia > 500 || valor_terapia > 1000)
+            {
+                descuento1 = valor_terapia * 0.30;
+            }
+            else
+            {
+                descuento1 = valor_terapia * 0.10;
+            }
+            return descuento1;
+        }
+
+        public string generarinforme(string[] nomPaciente, double[] descuento)
+        {
+            for (z = 0; z < limite; z++)
+            {
+                informe += "\nNombre del paciente: " + nomPaciente[z] + "\n Descuento: " + descuento[z];
+            }
+            return informe;
+        }
+    }
+
+}   
